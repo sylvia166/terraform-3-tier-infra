79e0bd842ed574d873d2c9103f604b6bbf921780
sylvia ndofung <sylviatchamba@gmail.com>
2023-11-13 11:07:55 -0500

    adding vpc, rds code

---
+++ locals.tf
@@ -0,0 +1,10 @@
+locals {
+  project_tags = {
+    contact = "devops@jjtech.com"
+    application = "payments"
+    project = "jjtech"
+    environment = "${terraform.workspace}"
+    creationTime = timestamp()
+
+  }
+}
